ed2d2c909f5afa428bcb63c47eb1a8190c43ddf4
zhullyb <zhullyb@outlook.com>
2024-12-03 13:52:24 +0800

    databuilder: modify remain rules

--- databuilder/preGenerator.py
+++ databuilder/preGenerator.py
@@ -1,16 +1,20 @@
 import json
 import time
+from datetime import datetime
 import concurrent.futures
 import yaml
 import feedparser
+from dateutil.relativedelta import relativedelta
 
 with open('../meta.yaml', 'r') as f:
     meta = yaml.full_load(f)
 
-articlesToRemain = 50
+Month = 4
+MinArticlesToRemain = 50
 threads = []
 opml = []
 data = []
+
 
 def main():
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
@@ -19,11 +23,24 @@ def main():
 
     data.sort(key=lambda x: x['date'], reverse=True)
 
+    # 做一个字符串比较，保留最近 4 个月的文章
+    dataFilteredByDate = list(filter(
+        lambda x: x['date'][:-2] >= (datetime.now() - relativedelta(months=Month)).strftime('%Y%m'),
+        data
+    ))
+
+    # 如果最近 4 个月的文章数量大于 50，则保留最近 4 个月的文章，否则保留最近 50 篇文章
+    if len(dataFilteredByDate) > MinArticlesToRemain:
+        dataToRemain = dataFilteredByDate
+    else:
+        dataToRemain = data[:MinArticlesToRemain]
+
     with open('../assets/data.json', 'w') as f:
-        json.dump(data[:articlesToRemain], f, indent=4, ensure_ascii=False)
+        json.dump(dataToRemain, f, indent=4, ensure_ascii=False)
 
     with open('../assets/opml.json', 'w') as f:
         json.dump(opml, f, indent=4, ensure_ascii=False)
+
 
 def fetch_feed(blog):
     try:
@@ -45,7 +62,7 @@ def fetch_feed(blog):
 
     if not alive:
         return
-    
+
     for entry in feed.entries:
         data.append({
             'title': entry['title'],
@@ -54,6 +71,7 @@ def fetch_feed(blog):
             'siteName': blog['title'],
             'siteUrl': blog['url']
         })
+
 
 if __name__ == '__main__':
     main()
