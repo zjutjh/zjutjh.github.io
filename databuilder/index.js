ded650531fcdd456ee25478b7375f2516f13e31f
FinleyGe <m13203533462@163.com>
2024-12-05 18:58:43 +0800

    feat: get data logic
    get data in 4 months,
    if the data in the last 4 months is not enough to 50 blogs,
    then get the latest 50 blogs

--- databuilder/index.js
+++ databuilder/index.js
@@ -1,9 +1,10 @@
 import yaml from "js-yaml";
 import * as fs from "fs";
 import { XMLParser } from "fast-xml-parser";
-import { format } from "date-fns";
+import { addMonths, format } from "date-fns";
 
 const articlesToRemain = parseInt(process.env.ARTICLES_TO_REMAIN ?? "50");
+const monthLimit = parseInt(process.env.MONTH_LIMIT ?? "4");
 
 /** 获取博客内容
  * @param {string} url 博客 URL
@@ -38,7 +39,8 @@ const { blogs } = yaml.load(fs.readFileSync("./meta.yaml", "utf8"));
       if (type === "rss") {
         data.push(...json.rss.channel.item.map((item) => ({
           title: item.title,
-          date: format(new Date(item.pubDate), "yyyyMMdd"),
+          date: new Date(item.pubDate),
+          // date: format(new Date(item.pubDate), "yyyyMMdd"),
           url: item.link,
           siteName: title,
           siteUrl: url
@@ -46,14 +48,16 @@ const { blogs } = yaml.load(fs.readFileSync("./meta.yaml", "utf8"));
       } else {
         data.push(...json.feed.entry.map((item) => ({
           title: item.title,
-          date: format(new Date(item.published), "yyyyMMdd"),
+          date: new Date(item.published),
+          // date: format(new Date(item.published), "yyyyMMdd"),
           url: item.link["_href"],
           siteName: title,
           siteUrl: url
         })));
       }
-    } catch (error) {
-      console.error(error, title);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (_error) {
+      console.error("获取失败", title, url);
       opml.push({
         title,
         link: url,
@@ -63,9 +67,20 @@ const { blogs } = yaml.load(fs.readFileSync("./meta.yaml", "utf8"));
   }
 
   data.sort((a, b) => new Date(b.date) - new Date(a.date));
-  const dataToWrite = data.slice(0, articlesToRemain);
-  // console.log(opml);
-  // console.log(data);
+  const monthData = addMonths(new Date(), monthLimit);
+  const dataToWrite = ((() => {
+    const dataToWrite = data.filter((item) => item.date >= monthData);
+    if (dataToWrite.length < articlesToRemain) {
+      return data.toSpliced(articlesToRemain);
+    }
+    return dataToWrite;
+  })()).map((item) => ({
+    title: item.title,
+    date: format(item.date, "yyyyMMdd"),
+    url: item.url,
+    siteName: item.siteName,
+    siteUrl: item.siteUrl
+  }));
   fs.writeFileSync("./assets/data.json", JSON.stringify(dataToWrite, null, 4));
   fs.writeFileSync("./assets/opml.json", JSON.stringify(opml, null, 4));
 })();
